031883d63cf869b4285217dc44aab1c9e50ecd84
Chris Godwin <patchshorts@gmail.com>
2020-05-18 08:57:58 -0500

    cond

--- webhook/docker-entrypoint.sh
+++ webhook/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 set -e
-
-for f in /docker-entrypoint.d/*.sh; do
-  # Don't print out any messages here since this is a CLI container
-  # chmod +x "$f"
-  "$f"
-done
+if [ $1 == "deploy" ];then
+  for f in /docker-entrypoint.d/*.sh; do
+    # Don't print out any messages here since this is a CLI container
+    # chmod +x "$f"
+    "$f"
+  done
 
 /usr/bin/r10k "$@" || exit 0
-
+fi
 exec /opt/puppetlabs/puppet/bin/ruby "/usr/local/bin/webhook"
